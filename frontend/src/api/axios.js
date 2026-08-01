3e1aa26416fcb1b9275cc7de59be84de9756c2df
Shreya Gardi <shreyaa.gardi09@gmail.com>
2026-08-01 15:06:39 +0530

    Update API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://blog-platform-epr1.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
